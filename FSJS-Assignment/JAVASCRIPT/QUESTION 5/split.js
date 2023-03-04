0d106f0172682de57f7f5724371c9a8646c0b760
iamaman526 <iamaman526@gmail.com>
2023-03-04 16:55:30 +0530

    question5-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 5/split.js
@@ -0,0 +1,4 @@
+//  05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+var stringtoArray =  "changetoarray";
+console.log(stringtoArray.split(''));
