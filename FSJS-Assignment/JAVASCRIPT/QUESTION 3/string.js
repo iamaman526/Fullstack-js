255553b577bf8b728b864b598f86e1c74b8888e2
iamaman526 <iamaman526@gmail.com>
2023-03-04 16:35:28 +0530

    question3-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 3/string.js
@@ -0,0 +1,4 @@
+//  03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+var Name = "aman kumar";
+console.log(Name.toUpperCase());
