ee7a206b102b86c3dfe0c9cc0eb9069b451ecbeb
iamaman526 <iamaman526@gmail.com>
2023-03-04 16:25:07 +0530

    question1-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 1/Variable.js
@@ -0,0 +1,13 @@
+// ### 01. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+var Name = 'aman';
+let age = 24;
+let isMarried = false;
+let team ;
+let team1 = null
+
+console.log(typeof Name);
+console.log(typeof isMarried);
+console.log(typeof age);
+console.log(typeof team);
+console.log(typeof team1);
