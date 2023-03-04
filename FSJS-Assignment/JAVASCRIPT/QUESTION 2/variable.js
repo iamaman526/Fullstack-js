6ba74d21fd78a047b19c7649a1ebc3e40c805739
iamaman526 <iamaman526@gmail.com>
2023-03-04 16:33:03 +0530

    question2-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 2/variable.js
@@ -0,0 +1,9 @@
+//  02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+var firstName = 'aman';
+var lastName = 'kumar';
+let country = 'india';
+let age = 24;
+let isMarried = false;
+
+console.log(`The firstName is: ${firstName} and the lastName is: ${lastName} and the country is: ${country} and the age is: ${age} and status of marriage is: ${isMarried} `);
