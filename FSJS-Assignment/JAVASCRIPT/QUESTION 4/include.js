54ba33e338248dac07bf0e5deb495f85e0cdc792
iamaman526 <iamaman526@gmail.com>
2023-03-04 16:47:38 +0530

    question4-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 4/include.js
@@ -0,0 +1,4 @@
+//  04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+var checkInclude = "java Script";
+console.log(checkInclude.includes("Script"));
