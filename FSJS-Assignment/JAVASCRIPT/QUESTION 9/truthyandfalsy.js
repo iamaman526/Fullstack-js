146682ac277e6bd3343f65ab950a8b0dd0f13d0b
iamaman526 <iamaman526@gmail.com>
2023-03-05 14:21:45 +0530

    question9-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 9/truthyandfalsy.js
@@ -0,0 +1,49 @@
+//  09. Boolean value is either true or false.
+
+//     - Write three JavaScript statement example which provide truthy value.
+
+var no = 4;
+
+if (no) {
+  console.log("The number is truthy.");
+} else {
+  console.log("The number is falsy.");
+}
+
+var string = "aman";
+if (string) {
+  console.log("The number is truthy.");
+} else {
+  console.log("The number is falsy.");
+}
+
+var bool = true;
+if (bool) {
+  console.log("The number is truthy.");
+} else {
+  console.log("The number is falsy.");
+}
+
+//     - Write three JavaScript statement example which provide falsy value.
+
+var no = 0;
+
+if (no) {
+  console.log("The number is truthy.");
+} else {
+  console.log("The number is falsy.");
+}
+
+var string = undefined;
+if (string) {
+  console.log("The number is truthy.");
+} else {
+  console.log("The number is falsy.");
+}
+
+var bool = false;
+if (bool) {
+  console.log("The number is truthy.");
+} else {
+  console.log("The number is falsy.");
+}
