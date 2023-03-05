38bc8d35ba8d2e310451c5b778dde08bc59bae97
iamaman526 <iamaman526@gmail.com>
2023-03-05 14:14:57 +0530

    question8-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 8/trim.js
@@ -0,0 +1,7 @@
+// 08. Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+let valueTotrim =  "  aman kumar   " 
+console.log(valueTotrim);
+
+console.log(valueTotrim.trim());
+
