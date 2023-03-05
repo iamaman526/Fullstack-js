2b019be623655c33f3df06029833b85d7cb5a04b
iamaman526 <iamaman526@gmail.com>
2023-03-05 14:26:19 +0530

    question10-done

---
+++ FSJS-Assignment/JAVASCRIPT/QUESTION 10/camparision.js
@@ -0,0 +1,36 @@
+// 10. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3
+//     - 4 >= 3
+//     - 4 < 3
+//     - 4 <= 3
+//     - 4 == 4
+//     - 4 === 4
+//     - 4 != 4
+//     - 4 !== 4
+//     - 4 != '4'
+//     - 4 == '4'
+//     - 4 === '4'
+  
+
+console.log(4 > 3);  //true
+console.log( 4 >= 3); // true
+console.log(4 < 3); // false
+console.log(4 <= 3); //false
+console.log(4 == 4);  // true
+console.log(4 === 4); //true
+console.log(4 != 4);  //false
+console.log(4 !== 4);  //false
+console.log(4 != '4');  //false
+console.log(4 == '4');  //true
+console.log(4 === '4'); //false
+
+// Find the length of python and jargon and make a falsy comparison statement.
+
+ var py = "python"
+ var ja = "jargon"
+ if (py.length === ja.length){
+  console.log("the statement is correct");
+ }
+ else{
+  console.log("the statment is not correct");
+ }
